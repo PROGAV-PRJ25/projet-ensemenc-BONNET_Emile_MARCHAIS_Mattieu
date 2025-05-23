0af9e22114a7208e85fb621554fe2eaa55406c73
Mindartis9 <mattieu.marchais@gmail.com>
2025-05-23 18:11:50 +0200

    Remove la music

--- Jeu1/Program.cs
+++ Jeu1/Program.cs
@@ -1,9 +1,5 @@
 ﻿
 Console.OutputEncoding = System.Text.Encoding.UTF8; //Pour l'affichage des émoticônes
 MainJeu mainJeu = new MainJeu();
-#if WINDOWS
-var player = new System.Media.SoundPlayer("tadow.wav");
-player.Play();
-#endif
 mainJeu.StartGame();
 mainJeu.AfficherFinJeu(mainJeu.ConditionFinDeJeu);
